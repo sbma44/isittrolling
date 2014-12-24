b05281587da9cac782c53cdbf820aed075bf1c2b
Tom Lee <thomas.j.lee@gmail.com>
2014-12-24 17:39:51 -0500

    echo progress

--- run.sh
+++ run.sh
@@ -1,206 +1,617 @@
+echo twas the night
 node index.js "twas the night" `cat num_minions.txt`
+
+echo before christmas
 node index.js "before christmas" `cat num_minions.txt`
+
+echo when all through
 node index.js "when all through" `cat num_minions.txt`
+
+echo the house not
 node index.js "the house not" `cat num_minions.txt`
+
+echo a creature
 node index.js "a creature" `cat num_minions.txt`
+
+echo was stirring
 node index.js "was stirring" `cat num_minions.txt`
+
+echo not even a
 node index.js "not even a" `cat num_minions.txt`
+
+echo mouse the stockings
 node index.js "mouse the stockings" `cat num_minions.txt`
+
+echo were hung by
 node index.js "were hung by" `cat num_minions.txt`
+
+echo the chimney
 node index.js "the chimney" `cat num_minions.txt`
+
+echo with care in
 node index.js "with care in" `cat num_minions.txt`
+
+echo hopes that
 node index.js "hopes that" `cat num_minions.txt`
+
+echo st nicholas
 node index.js "st nicholas" `cat num_minions.txt`
+
+echo soon would
 node index.js "soon would" `cat num_minions.txt`
+
+echo be there the
 node index.js "be there the" `cat num_minions.txt`
+
+echo children were
 node index.js "children were" `cat num_minions.txt`
+
+echo nestled all
 node index.js "nestled all" `cat num_minions.txt`
+
+echo snug in their
 node index.js "snug in their" `cat num_minions.txt`
+
+echo beds while
 node index.js "beds while" `cat num_minions.txt`
+
+echo visions of
 node index.js "visions of" `cat num_minions.txt`
+
+echo sugar plums
 node index.js "sugar plums" `cat num_minions.txt`
+
+echo danced in their
 node index.js "danced in their" `cat num_minions.txt`
+
+echo heads and mamma
 node index.js "heads and mamma" `cat num_minions.txt`
+
+echo in her kerchief
 node index.js "in her kerchief" `cat num_minions.txt`
+
+echo and i in my
 node index.js "and i in my" `cat num_minions.txt`
+
+echo cap had just
 node index.js "cap had just" `cat num_minions.txt`
+
+echo settled our
 node index.js "settled our" `cat num_minions.txt`
+
+echo brains for
 node index.js "brains for" `cat num_minions.txt`
+
+echo a long winters
 node index.js "a long winters" `cat num_minions.txt`
+
+echo nap when
 node index.js "nap when" `cat num_minions.txt`
+
+echo out on the
 node index.js "out on the" `cat num_minions.txt`
+
+echo lawn there
 node index.js "lawn there" `cat num_minions.txt`
+
+echo arose such
 node index.js "arose such" `cat num_minions.txt`
+
+echo a clatter i
 node index.js "a clatter i" `cat num_minions.txt`
+
+echo sprang from
 node index.js "sprang from" `cat num_minions.txt`
+
+echo my bed to see
 node index.js "my bed to see" `cat num_minions.txt`
+
+echo what was the
 node index.js "what was the" `cat num_minions.txt`
+
+echo matter away
 node index.js "matter away" `cat num_minions.txt`
+
+echo to the window
 node index.js "to the window" `cat num_minions.txt`
+
+echo i flew like
 node index.js "i flew like" `cat num_minions.txt`
+
+echo a flash tore
 node index.js "a flash tore" `cat num_minions.txt`
+
+echo open the shutters
 node index.js "open the shutters" `cat num_minions.txt`
+
+echo and threw up
 node index.js "and threw up" `cat num_minions.txt`
+
+echo the sash the
 node index.js "the sash the" `cat num_minions.txt`
+
+echo moon on the
 node index.js "moon on the" `cat num_minions.txt`
+
+echo breast of the
 node index.js "breast of the" `cat num_minions.txt`
+
+echo new fallen
 node index.js "new fallen" `cat num_minions.txt`
+
+echo snow gave a
 node index.js "snow gave a" `cat num_minions.txt`
+
+echo lustre of midday
 node index.js "lustre of midday" `cat num_minions.txt`
+
+echo to objects
 node index.js "to objects" `cat num_minions.txt`
+
+echo below when
 node index.js "below when" `cat num_minions.txt`
+
+echo what to my
 node index.js "what to my" `cat num_minions.txt`
+
+echo wondering eyes
 node index.js "wondering eyes" `cat num_minions.txt`
+
+echo did appear
 node index.js "did appear" `cat num_minions.txt`
+
+echo but a miniature
 node index.js "but a miniature" `cat num_minions.txt`
+
+echo sleigh and
 node index.js "sleigh and" `cat num_minions.txt`
+
+echo eight tiny
 node index.js "eight tiny" `cat num_minions.txt`
+
+echo reindeer with
 node index.js "reindeer with" `cat num_minions.txt`
+
+echo a little old
 node index.js "a little old" `cat num_minions.txt`
+
+echo driver so lively
 node index.js "driver so lively" `cat num_minions.txt`
+
+echo and quick i
 node index.js "and quick i" `cat num_minions.txt`
+
+echo knew in a moment
 node index.js "knew in a moment" `cat num_minions.txt`
+
+echo he must be
 node index.js "he must be" `cat num_minions.txt`
+
+echo st nick
 node index.js "st nick" `cat num_minions.txt`
+
+echo more rapid than
 node index.js "more rapid than" `cat num_minions.txt`
+
+echo eagles his
 node index.js "eagles his" `cat num_minions.txt`
+
+echo coursers they
 node index.js "coursers they" `cat num_minions.txt`
+
+echo came and he
 node index.js "came and he" `cat num_minions.txt`
+
+echo whistled and
 node index.js "whistled and" `cat num_minions.txt`
+
+echo shouted and
 node index.js "shouted and" `cat num_minions.txt`
+
+echo called them
 node index.js "called them" `cat num_minions.txt`
+
+echo by name
 node index.js "by name" `cat num_minions.txt`
+
+echo now dasher
 node index.js "now dasher" `cat num_minions.txt`
+
+echo now dancer
 node index.js "now dancer" `cat num_minions.txt`
+
+echo now prancer
 node index.js "now prancer" `cat num_minions.txt`
+
+echo and vixen
 node index.js "and vixen" `cat num_minions.txt` 
+
+echo on comet
 node index.js "on comet" `cat num_minions.txt`
+
+echo on cupid
 node index.js "on cupid" `cat num_minions.txt`
+
+echo on donner
 node index.js "on donner" `cat num_minions.txt`
+
+echo and blitzen
 node index.js "and blitzen" `cat num_minions.txt`
+
+echo to the top of
 node index.js "to the top of" `cat num_minions.txt`
+
+echo the porch
 node index.js "the porch" `cat num_minions.txt`
+
+echo to the top of
 node index.js "to the top of" `cat num_minions.txt`
+
+echo the wall now
 node index.js "the wall now" `cat num_minions.txt`
+
+echo dash away dash
 node index.js "dash away dash" `cat num_minions.txt`
+
+echo away dash away
 node index.js "away dash away" `cat num_minions.txt`
+
+echo all as leaves
 node index.js "all as leaves" `cat num_minions.txt`
+
+echo that before
 node index.js "that before" `cat num_minions.txt`
+
+echo the wild hurricane
 node index.js "the wild hurricane" `cat num_minions.txt`
+
+echo fly when they
 node index.js "fly when they" `cat num_minions.txt`
+
+echo meet with an
 node index.js "meet with an" `cat num_minions.txt`
+
+echo obstacle mount
 node index.js "obstacle mount" `cat num_minions.txt`
+
+echo to the sky
 node index.js "to the sky" `cat num_minions.txt`
+
+echo so up to the
 node index.js "so up to the" `cat num_minions.txt`
+
+echo housetop the
 node index.js "housetop the" `cat num_minions.txt`
+
+echo coursers they
 node index.js "coursers they" `cat num_minions.txt`
+
+echo flew with the
 node index.js "flew with the" `cat num_minions.txt`
+
+echo sleigh full
 node index.js "sleigh full" `cat num_minions.txt`
+
+echo of toys and
 node index.js "of toys and" `cat num_minions.txt`
+
+echo st nicholas
 node index.js "st nicholas" `cat num_minions.txt`
+
+echo too and then
 node index.js "too and then" `cat num_minions.txt`
+
+echo in a twinkling
 node index.js "in a twinkling" `cat num_minions.txt`
+
+echo i heard on
 node index.js "i heard on" `cat num_minions.txt`
+
+echo the roof the
 node index.js "the roof the" `cat num_minions.txt`
+
+echo prancing and
 node index.js "prancing and" `cat num_minions.txt`
+
+echo pawing of each
 node index.js "pawing of each" `cat num_minions.txt`
+
+echo little hoof
 node index.js "little hoof" `cat num_minions.txt`
+
+echo as i drew in
 node index.js "as i drew in" `cat num_minions.txt`
+
+echo my head and
 node index.js "my head and" `cat num_minions.txt`
+
+echo was turning
 node index.js "was turning" `cat num_minions.txt`
+
+echo around down
 node index.js "around down" `cat num_minions.txt`
+
+echo the chimney
 node index.js "the chimney" `cat num_minions.txt`
+
+echo st nicholas
 node index.js "st nicholas" `cat num_minions.txt`
+
+echo came with a
 node index.js "came with a" `cat num_minions.txt`
+
+echo bound he was
 node index.js "bound he was" `cat num_minions.txt`
+
+echo dressed all
 node index.js "dressed all" `cat num_minions.txt`
+
+echo in fur from
 node index.js "in fur from" `cat num_minions.txt`
+
+echo his head to
 node index.js "his head to" `cat num_minions.txt`
+
+echo his foot and
 node index.js "his foot and" `cat num_minions.txt`
+
+echo his clothes
 node index.js "his clothes" `cat num_minions.txt`
+
+echo were all tarnished
 node index.js "were all tarnished" `cat num_minions.txt`
+
+echo with ashes
 node index.js "with ashes" `cat num_minions.txt`
+
+echo and soot a
 node index.js "and soot a" `cat num_minions.txt`
+
+echo bundle of toys
 node index.js "bundle of toys" `cat num_minions.txt`
+
+echo he had flung
 node index.js "he had flung" `cat num_minions.txt`
+
+echo on his back
 node index.js "on his back" `cat num_minions.txt`
+
+echo and he looked
 node index.js "and he looked" `cat num_minions.txt`
+
+echo like a pedler
 node index.js "like a pedler" `cat num_minions.txt`
+
+echo just opening
 node index.js "just opening" `cat num_minions.txt`
+
+echo his pack his
 node index.js "his pack his" `cat num_minions.txt`
+
+echo eyes how they
 node index.js "eyes how they" `cat num_minions.txt`
+
+echo twinkled his
 node index.js "twinkled his" `cat num_minions.txt`
+
+echo dimples how
 node index.js "dimples how" `cat num_minions.txt`
+
+echo merry his cheeks
 node index.js "merry his cheeks" `cat num_minions.txt`
+
+echo were like roses
 node index.js "were like roses" `cat num_minions.txt`
+
+echo his nose like
 node index.js "his nose like" `cat num_minions.txt`
+
+echo a cherry his
 node index.js "a cherry his" `cat num_minions.txt`
+
+echo droll little
 node index.js "droll little" `cat num_minions.txt`
+
+echo mouth was drawn
 node index.js "mouth was drawn" `cat num_minions.txt`
+
+echo up like a bow
 node index.js "up like a bow" `cat num_minions.txt`
+
+echo and the beard
 node index.js "and the beard" `cat num_minions.txt`
+
+echo on his chin
 node index.js "on his chin" `cat num_minions.txt`
+
+echo was as white
 node index.js "was as white" `cat num_minions.txt`
+
+echo as the snow
 node index.js "as the snow" `cat num_minions.txt`
+
+echo the stump of
 node index.js "the stump of" `cat num_minions.txt`
+
+echo a pipe he held
 node index.js "a pipe he held" `cat num_minions.txt`
+
+echo tight in his
 node index.js "tight in his" `cat num_minions.txt`
+
+echo teeth and the
 node index.js "teeth and the" `cat num_minions.txt`
+
+echo smoke it encircled
 node index.js "smoke it encircled" `cat num_minions.txt`
+
+echo his head like
 node index.js "his head like" `cat num_minions.txt`
+
+echo a wreath he
 node index.js "a wreath he" `cat num_minions.txt`
+
+echo had a broad
 node index.js "had a broad" `cat num_minions.txt`
+
+echo face and a
 node index.js "face and a" `cat num_minions.txt`
+
+echo little round
 node index.js "little round" `cat num_minions.txt`
+
+echo belly that
 node index.js "belly that" `cat num_minions.txt`
+
+echo shook when
 node index.js "shook when" `cat num_minions.txt`
+
+echo he laughed
 node index.js "he laughed" `cat num_minions.txt`
+
+echo like a bowl
 node index.js "like a bowl" `cat num_minions.txt`
+
+echo full of jelly
 node index.js "full of jelly" `cat num_minions.txt`
+
+echo he was chubby
 node index.js "he was chubby" `cat num_minions.txt`
+
+echo and plump a
 node index.js "and plump a" `cat num_minions.txt`
+
+echo right jolly
 node index.js "right jolly" `cat num_minions.txt`
+
+echo old elf and
 node index.js "old elf and" `cat num_minions.txt`
+
+echo i laughed when
 node index.js "i laughed when" `cat num_minions.txt`
+
+echo i saw him in
 node index.js "i saw him in" `cat num_minions.txt`
+
+echo spite of myself
 node index.js "spite of myself" `cat num_minions.txt`
+
+echo a wink of his
 node index.js "a wink of his" `cat num_minions.txt`
+
+echo eye and a twist
 node index.js "eye and a twist" `cat num_minions.txt`
+
+echo of his head
 node index.js "of his head" `cat num_minions.txt`
+
+echo soon gave me
 node index.js "soon gave me" `cat num_minions.txt`
+
+echo to know i had
 node index.js "to know i had" `cat num_minions.txt`
+
+echo nothing to
 node index.js "nothing to" `cat num_minions.txt`
+
+echo dread he spoke
 node index.js "dread he spoke" `cat num_minions.txt`
+
+echo not a word
 node index.js "not a word" `cat num_minions.txt`
+
+echo but went straight
 node index.js "but went straight" `cat num_minions.txt`
+
+echo to his work
 node index.js "to his work" `cat num_minions.txt`
+
+echo and filled
 node index.js "and filled" `cat num_minions.txt`
+
+echo all the stockings
 node index.js "all the stockings" `cat num_minions.txt`
+
+echo then turned
 node index.js "then turned" `cat num_minions.txt`
+
+echo with a jerk
 node index.js "with a jerk" `cat num_minions.txt`
+
+echo and laying
 node index.js "and laying" `cat num_minions.txt`
+
+echo his finger
 node index.js "his finger" `cat num_minions.txt`
+
+echo aside of his
 node index.js "aside of his" `cat num_minions.txt`
+
+echo nose and giving
 node index.js "nose and giving" `cat num_minions.txt`
+
+echo a nod up the
 node index.js "a nod up the" `cat num_minions.txt`
+
+echo chimney he
 node index.js "chimney he" `cat num_minions.txt`
+
+echo rose he sprang
 node index.js "rose he sprang" `cat num_minions.txt`
+
+echo to his sleigh
 node index.js "to his sleigh" `cat num_minions.txt`
+
+echo to his team
 node index.js "to his team" `cat num_minions.txt`
+
+echo gave a whistle
 node index.js "gave a whistle" `cat num_minions.txt`
+
+echo and away they
 node index.js "and away they" `cat num_minions.txt`
+
+echo all flew like
 node index.js "all flew like" `cat num_minions.txt`
+
+echo the down of
 node index.js "the down of" `cat num_minions.txt`
+
+echo a thistle but
 node index.js "a thistle but" `cat num_minions.txt`
+
+echo i heard him
 node index.js "i heard him" `cat num_minions.txt`
+
+echo exclaim ere
 node index.js "exclaim ere" `cat num_minions.txt`
+
+echo he drove out
 node index.js "he drove out" `cat num_minions.txt`
+
+echo of sight
 node index.js "of sight" `cat num_minions.txt`
+
+echo happy christmas
 node index.js "happy christmas" `cat num_minions.txt`
+
+echo to all and
 node index.js "to all and" `cat num_minions.txt`
+
+echo to all
 node index.js "to all" `cat num_minions.txt`
+
+echo a good night
 node index.js "a good night" `cat num_minions.txt`
+
+echo a good night
 node index.js "a good night" `cat num_minions.txt`
+
+echo a good night
 node index.js "a good night" `cat num_minions.txt`
+
+echo a good night
 node index.js "a good night" `cat num_minions.txt`
+
+echo a good night
 node index.js "a good night" `cat num_minions.txt`
